c87a81c84d392ac18f5793af3215e26f08ff9d0b
yuanenzhi <710575900@qq.com>
2019-10-12 15:22:02 +0800

    ...

---
+++ mall-enhance/src/main/java/com/yuan/mallfastmybatis/MallFastmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.mallfastmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallFastmybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallFastmybatisApplication.class, args);
+    }
+
+}
